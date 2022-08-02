23a4508ef46b2c8ab9c3e76944b874d72c289494
Chrissolo88 <chrissolo88@live.com>
2022-08-02 16:12:10 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Timers/timersExercise.js

--- Timers/timersExercise.js
+++ Timers/timersExercise.js
@@ -8,5 +8,17 @@ function countdown(count) {
             console.log("DONE!");
         }
     }, 1000);
+}
 
+function randomgame() {
+    let num = 0;
+    let count = 0;
+    let interval = setInterval(function() {
+        num = Math.random();
+        count++;
+        if(num > .75) {
+            clearInterval(interval);
+            console.log(`Number: ${num}, it took ${count} attempts to get a number greater than .75`);
+        }
+    }, 1000);
 }
